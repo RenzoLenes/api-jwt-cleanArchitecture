3833eb4b4b76b48ca1ad8ac5ad3ec5002097d373
Renzo Lenes <lenesrenzoalberto@gmail.com>
2025-01-27 10:03:55 -0500

    initial commit

---
+++ app/app.py
@@ -0,0 +1,10 @@
+from fastapi import FastAPI
+from routers import demo, security, user_management
+from repositories.database import Database
+
+app = FastAPI()
+app.include_router(demo.router)
+app.include_router(security.router)
+app.include_router(user_management.router)
+
+Database.initialize('my-db-name')
---
+++ app/repositories/user_repository.py
@@ -0,0 +1,21 @@
+from fastapi import Depends
+
+from .database import Database, BaseRepository
+from documents.user_documents import UserDocument
+
+
+class UserRepository(BaseRepository):
+    def __init__(self, db: Database = Depends()):
+        super().__init__(db)
+
+    @property
+    def _collection(self):
+        return 'user'
+
+    def find_one_by_username(self, username) -> UserDocument | None:
+        result = self._db.find_one(self._collection, {'username': username})
+        if not result:
+            return None
+        print(result)
+        user = UserDocument(**result, user_id=str(result['_id']))
+        return user
